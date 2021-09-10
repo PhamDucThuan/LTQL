b6d27f0f40c32f76087c083d278f7f64a76b6acd
PHAM DUC THUAN <1721050507@student.humg.edu.vn>
2021-09-10 15:08:01 +0700

    LTQL

--- LTQL/Models/Student.cs
+++ LTQL/Models/Student.cs
@@ -12,7 +12,10 @@ namespace LTQL.Models
     {
         [Key]
         public string StudentID { get; set; }
+        [Required(ErrorMessage = "Student name is required")]
+        [MinLength(3)]
         public string StudentName { get; set; }
+        [Required]
         public string Address { get; set; }
     }
 }
